edaba55ae4009ea38ea293b41c93252cb9278643
McCannAaron <mccannaaront@gmail.com>
2023-09-02 16:39:58 -0400

    Added setters

--- Dog.java
+++ Dog.java
@@ -16,18 +16,26 @@ public class Dog {
 
     public String getName() { return name; }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Color getColor() { return color; }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 
     public int getAge() { return age; }
 
+    public void setAge(int age) {
+        this.age =age;
+    }
+
     public Breed getBreed() { return breed; }
 
-    public void setColor(Color color) {
+    public void setBreed(Breed breed) {
+        this.breed = breed;
     }
 
-    public void setAge(int age) {
-    }
-
-    public void setBreed(Breed breed){
-    }
 }
--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
         Dog dog2 = new Dog("Spot", Color.BLACK, 5, Breed.DACHSHUND);
         Dog dog3 = new Dog("Wolf", Color.YELLOW, 3, Breed.GOLDEN_RETRIEVER);
         System.out.println("Dog name: " + dog1.getName());
-        System.out.println(" ");
-        System.out.println("Dog color: " + dog1.getColor());
+        dog1.setName("Snoppy");
+        System.out.println("New name: " + dog1.getName());
     }
 }
