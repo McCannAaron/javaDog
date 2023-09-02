d117bb1691f409d36962e68221b00fdbdaaf5095
McCannAaron <mccannaaront@gmail.com>
2023-09-02 16:31:54 -0400

    Added getters

--- Dog.java
+++ Dog.java
@@ -1,4 +1,33 @@
 public class Dog {
-    public Dog(String name, Color color, int i, Breed breed) {
+
+    private String name;
+
+    private Color color;
+    private int age;
+
+    private Breed breed;
+
+    public Dog(String name, Color color, int age, Breed breed) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public String getName() { return name; }
+
+    public Color getColor() { return color; }
+
+    public int getAge() { return age; }
+
+    public Breed getBreed() { return breed; }
+
+    public void setColor(Color color) {
+    }
+
+    public void setAge(int age) {
+    }
+
+    public void setBreed(Breed breed){
     }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog1 = new Dog("Snoppy", Color.YELLOW, 9, Breed.YELLOW_LAB);
+        Dog dog1 = new Dog("Roger", Color.YELLOW, 9, Breed.YELLOW_LAB);
         Dog dog2 = new Dog("Spot", Color.BLACK, 5, Breed.DACHSHUND);
         Dog dog3 = new Dog("Wolf", Color.YELLOW, 3, Breed.GOLDEN_RETRIEVER);
+        System.out.println("Dog name: " + dog1.getName());
+        System.out.println(" ");
+        System.out.println("Dog color: " + dog1.getColor());
     }
 }
